7c5d09641e84312a93d80a58fd63aab139c1e3e7
Juhani <juhanirusi@yahoo.com>
2022-01-31 15:03:34 +0200

    Lisätty kysymyksiä.

---
+++ kysymykset.py
@@ -0,0 +1,26 @@
+import html
+import requests
+import random
+
+API_OSOITE = "https://opentdb.com/api.php?amount=10&type=multiple&difficulty=easy"
+
+def lataa_kysymykset_netista():
+    vastaus = requests.get(API_OSOITE)
+    tiedot = vastaus.json()
+    kysymykset_ja_vastaukset = []
+    for juttu in tiedot["results"]:
+        kysymys = juttu["question"]
+        oikea_vastaus = juttu["correct_answer"]
+        vaarat_vastaukset = juttu["incorrect_answers"]
+        vastaukset = ["*" + oikea_vastaus] + vaarat_vastaukset
+        random.shuffle(vastaukset)
+        tekstit = [
+            html.unescape(teksti)
+            for teksti in [kysymys] + vastaukset
+        ]
+        kysymykset_ja_vastaukset.append(tekstit)
+    return kysymykset_ja_vastaukset
+    
+if __name__ == "__main__":
+    import pprint
+    pprint.pprint(lataa_kysymykset_netista())
--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 import numbers
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from kysymykset import lataa_kysymykset_netista
 
 from quiz_ui import Ui_MainWindow
-
-KYSYMYKSET_JA_VASTAUKSET = [
-    (
-        "Mistä Python-ohjelmointikieli on saanut nimensä?",
-        "käärmeestä",
-        "laulusta",
-        "*TV-sarjasta",
-        "elokuvasta",
-    ),
-    (
-        "Paljonko on 5*5?",
-        "22",
-        "*25",
-        "27",
-        "35",
-    )
-]
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.tiedot = lataa_kysymykset_netista()
         self.vaihda_kysymys_ja_vastaukset(0)
+        self.kytke_napit()
+        self.pisteet = 0
+        self.indeksi = 0
 
     def vaihda_kysymys_ja_vastaukset(self, indeksi):
-        tekstit = KYSYMYKSET_JA_VASTAUKSET[indeksi]
+        tekstit = self.tiedot[indeksi]
         uudet_tekstit = []
         for (numero, teksti) in enumerate(tekstit):
             if teksti.startswith('*'):
-                teksti = teksti[1:]
+                #teksti = teksti[1:]
                 self.oikea_vastaus = numero
             uudet_tekstit.append(teksti)
         self.aseta_tekstit(uudet_tekstit)
@@ -52,6 +39,39 @@ class MainWindow(QMainWindow):
 
     def aseta_kysymys(self, kysymys):
         self.ui.label.setText(kysymys)
+        
+    def kytke_napit(self):
+        self.ui.button1.clicked.connect(self.nappia_painettu)
+        self.ui.button2.clicked.connect(self.nappia_painettu)
+        self.ui.button3.clicked.connect(self.nappia_painettu)
+        self.ui.button4.clicked.connect(self.nappia_painettu)
+          
+    def nappia_painettu(self):
+        if self.sender() == self.ui.button1:
+            nappi = 1
+        elif self.sender() == self.ui.button2:
+            nappi = 2
+        elif self.sender() == self.ui.button3:
+            nappi = 3
+        elif self.sender() == self.ui.button4:
+            nappi = 4
+        else:
+            return
+        
+        if nappi == self.oikea_vastaus:
+            print("OIKEIN!")
+            self.pisteet += 1
+        
+        self.indeksi += 1
+        if self.indeksi >= len(self.tiedot):
+            laatikko = QMessageBox(self)
+            laatikko.setText(f"Peli päättyi Sait {self.pisteet} pistettä.")
+            laatikko.exec()
+            self.indeksi = 0
+            self.pisteet = 0
+        
+        self.vaihda_kysymys_ja_vastaukset(self.indeksi)
+        
 
 
 if __name__ == "__main__":
